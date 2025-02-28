9aab6e92ff436f802315428cc0b3ecca333a19fb
TalisonDS <talison.mateuscm@gmail.com>
2025-02-28 12:12:30 -0300

    inicio da função adicionarAmigo

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 let amigos = []
 
 function adicionarAmigo() {
-    console.log('O botão está funcionando!')
-    amigos = document.querySelector ('input').value
-    console.log(amigos)
+    const input = document.getElementById('amigo');
+    let nome = input.value.trim();
+
+    if (nome === '' || !isNaN(nome)) {
+        alert('Por favor, insira um nome válido.');
+        return;
+    }
+
+    amigos.push(nome);
+    
 }
